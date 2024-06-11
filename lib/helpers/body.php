14d705123843bf4670acd9a53f0c1cf3accbcebf
ulikemywa1 <calium@outlook.com>
2024-06-11 14:42:18 +0300

    feat: implement preloader functionality

--- lib/helpers/body.php
+++ lib/helpers/body.php
@@ -9,6 +9,6 @@ function hm_custom_body_content() {
 			<lottie-player src="<?php bloginfo('template_directory'); ?>/resources/lottie/preloader.json" class="lottie-player" background="transparent" speed="1" loop autoplay></lottie-player>
 		</div><!-- /.loader-lottie -->
 	</div>
-
+	<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 	<?php
 }
